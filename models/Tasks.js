5da3f32f19a1a6425b0732bdfcb1c228af6a497d
kathecorner <40414604+kathecorner@users.noreply.github.com>
2023-07-25 14:39:57 +0900

    Update Tasks.js

--- models/Tasks.js
+++ models/Tasks.js
@@ -1,1 +1,16 @@
-mode
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Enter task name"],
+    trim: true,
+    maxlength: [20, "The task name has to be less than 20 letters."],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
